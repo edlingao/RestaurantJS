ee31d1ae10ed4c1b421f9350f64a64a0bd6fcf9d
edlingao <sonido244@gmail.com>
2019-12-19 22:12:09 -0600

    feat(contact):Added contact page

---
+++ README.md
@@ -0,0 +1,11 @@
+# Restaurant page
+
+Page created using only JS and DOM manipulation
+
+##### To initiate the project
+* npm install
+* npm run build 
+
+##### To see the result 
+* Install live server on VSCODE
+* Or use a realtime server to visualite the html page
--- src/index.js
+++ src/index.js
@@ -75,6 +75,54 @@ const foodMenu = () => {
     return foodMenuContainer;
 }
 
+const contact = () => {
+    const navigation = document.createElement('i');
+    const phone = document.createElement('i');
+    const location = document.createElement('div');
+    const contact = document.createElement('div');
+    const container = document.createElement('div');
+    const locationTitle = document.createElement('h1');
+    const contactTitle = document.createElement('h1');
+    const direction = document.createElement('p');
+    const directionSecondParagraph = document.createElement('p');
+    const contactInfo = document.createElement('p');
+    const contactInfoSecondParagraph = document.createElement('p');
+    
+
+    location.classList.add('location');
+    contact.classList.add('contact');
+    container.classList.add('contact-container');
+
+    navigation.classList.add('material-icons');
+    navigation.innerHTML = 'navigation';
+
+    phone.classList.add('material-icons');
+    phone.innerHTML = 'phone';
+
+    direction.append(`396 Single Street, Needham City, MA 02192`);
+    directionSecondParagraph.append('Opening Hours: 12:00 PM - 11:00 PM');
+    contactInfo.append(`email: theburgerplace@email.com`);
+    contactInfoSecondParagraph.append('tel: 123-456-7890');
+    
+    locationTitle.appendChild(navigation);
+    locationTitle.append('Location');
+    contactTitle.appendChild(phone);
+    contactTitle.append('Contact');
+
+    location.appendChild(locationTitle);
+    location.appendChild(direction);
+    location.appendChild(directionSecondParagraph);
+
+    contact.appendChild(contactTitle);
+    contact.appendChild(contactInfo);
+    contact.appendChild(contactInfoSecondParagraph);
+
+    container.appendChild(location);
+    container.appendChild(contact);
+
+    return container;
+}
+
 const pagesContainer = (page)=>{
     let redBox = document.createElement('div');
     let logo = document.createElement('div');
@@ -114,7 +162,8 @@ const main = () => {
         changePage(container, foodMenu());
     });
     contacButton.addEventListener('click', ()=>{
-
+        removePage(container);
+        changePage(container, contact());
     });
     
     
